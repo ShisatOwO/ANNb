implemented all methods and fixed some mistakes with the derivatives of Relu and Softmax

annb/actfunc.py
@@ -1,24 +1,40 @@
 import numpy as np
 
-from abc import ABC, abstractmethod
+from annb.templates import *
 
 
-class Actfunc(ABC):
+class Linear(Actfunc):
     @staticmethod
-    @abstractmethod
-    def forward(inp: np.array) -> np.array:
-        ...
+    def forward(x: np.array, reference: np.array = None, raw_lout: np.array = None) -> np.array:
+        return x
 
     @staticmethod
-    def backward(dinp: np.array) -> np.array:
+    def backward(dinp: np.array, reference: np.array = None, raw_lout: np.array = None) -> np.array:
         return 1
 
 
-class CCESoftmax(Actfunc):
-    ...
-
 class Relu(Actfunc):
-    ...
+    @staticmethod
+    def forward(x: np.array, reference: np.array = None, raw_lout: np.array = None) -> np.array:
+        return np.maximum(0, x)
 
-class Linear(Actfunc):
-    ...
+    @staticmethod
+    def backward(x: np.array, reference: np.array = None, raw_lout: np.array = None) -> np.array:
+        out = np.copy(x)
+        out[raw_lout <= 0] = 0
+        return out
+
+
+class Softmax(Actfunc):
+    @staticmethod
+    def forward(inp: np.array, reference: np.array = None, raw_lout: np.array = None) -> np.array:
+        exp = np.exp(inp - np.max(inp, axis=1, keepdims=True))
+        return exp / np.sum(exp, axis=1, keepdims=True)
+
+    @staticmethod
+    def backward(dinp: np.array, reference: np.array = None, raw_lout: np.array = None):
+        x = np.argmax(reference, axis=1) if len(reference.shape) == 2 else reference
+        out = dinp.copy()
+        out[range(len(reference)), x] -= 1
+        return out / len(reference)
+

annb/layers.py
@@ -1,32 +1,34 @@
 import numpy as np
 
 from dataclasses import dataclass
-from abc import ABC, abstractmethod
 
 from annb.actfunc import Actfunc
 from annb.optimizer import *
+from annb.templates import *
 
 
-class Layer(ABC):
-    @abstractmethod
+class DenseLayer(Layer):
     def __init__(self, connections: int, neurons: int) -> None:
-        self.out: np.array = None
+        super().__init__(connections, neurons)
 
-        self._weights = np.random.randn(connections, neurons) / 100
-
-    @abstractmethod
     def fpass(self, inp: np.array, actfunc: Actfunc) -> np.array:
-        ...
+        self._inp = inp
+        self.raw_out = np.dot(inp, self._weights) + self._bias
+        self.out = actfunc.forward(self.raw_out)
+        return self.out
 
-    def bpass(self, dinp: np.array, actfunc: Actfunc) -> None:
-        ...
+    def bpass(self, dinp: np.array, actfunc: Actfunc, reference: np.array) -> np.array:
+        din = actfunc.backward(dinp, reference, self.raw_out)
+        self._dw = np.dot(self._inp.T, din)
+        self._db = np.sum(din, axis=0, keepdims=1)
+        self._inp = np.dot(din, self._weights.T)
+        return self._inp
 
-    def optimize(self, optimizer: Optimizer, gradient: [np.array, np.array]):
-        ...
+    def optimize(self, optimizer: Optimizer, learning_rate: float) -> None:
+        optimizer.optimize(self, learning_rate)
 
 
 @dataclass
 class LayerInfo:
     layer: Layer
     actfunc: Actfunc
-    gradient: [np.array, np.array] = None

annb/n_manager.py
@@ -18,14 +18,32 @@ class NManager:
         self.out = inp
         return inp
 
-    def optimize(self) -> None:
-        ...
+    def optimize(self, optimizer: Optimizer, learning_rate: float = 1) -> None:
+        for layer in self._layers:
+            layer.layer.optimize(optimizer, learning_rate)
 
     def calc_loss(self, reference: np.array) -> float:
-        ...
+        # Es könnte vorkommen, dass prediction eine Null enthält. Deshalb werden werden alle Werte, die kleiner als 10^-9,
+        # Auf ein Milliardstel gesetzt (10^-9). Wenn ein wert Null wäre gabe es ein Fehler bei der Log Funktion.
+        prediction = np.clip(self.out, 1e-7, 1 - 1e-7)
+
+        # Die Dimensionen von goal erweitern, damit es zu den dimensionen der Prediction passt.
+        ggoal = []
+        for ref in reference:
+            t = [0] * (prediction.shape[1] - 1)
+            t.insert(ref, 1)
+            ggoal.append(t)
+        loss = -np.sum(np.log(prediction) * ggoal, axis=1)
+        return loss
 
     def calc_acc(self, reference: np.array) -> float:
-        ...
+        pred = np.argmax(self.out, axis=1)
+        x = np.argmax(reference, axis=1) if len(reference.shape) == 2 else reference
+        return np.mean(reference==pred)
 
-    def calc_gradient(self, reference: np.array) -> None:
-        ...
+    def calc_gradient(self, reference: np.array) -> float:
+        dinp = self.out
+        for layer in self._layers[::-1]:
+            dinp = layer.layer.bpass(dinp, layer.actfunc, reference)
+
+        return np.mean(self.calc_loss(reference))